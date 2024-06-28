5c322fd1f4b33008b2389f03cc5f8d3afe69341a
maddiepolsky <madison_polsky@branson.org>
2024-06-28 14:00:47 -0700

    Add files via upload

---
+++ wk2_problem_3.py
@@ -0,0 +1,14 @@
+
+#not finished
+
+arr_1 = [1,2,3,4,5,6,7,8,9,10]
+arr_2 = [1,2,3,4,5,6,7,8,9,10]
+
+target_int = 10
+
+for num1 in arr_1:
+    for num2 in arr_2:
+        if num1 + num2 == target_int:
+            print ("true")
+        else:
+            print ("false")
